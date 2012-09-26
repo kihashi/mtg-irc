52646c0f69177915ca6768a9e2973bfe472e0c6d
John Cleaver <cleaver.john.k@gmail.com>
2012-09-26 11:42:40 -0400

    Added a basic card function that displays text.

--- card.py
+++ card.py
@@ -1,7 +1,14 @@
 '''
+card.py - Phenny Magic: the Gathering Card Lookup Module
 A module for the IRC Bot Phenny that allows for one to get the Oracle text
 for any Magic: the Gathering card by card name.
 
 Author: John Cleaver
 Site: https://github.com/kihashi/mtg-irc
+License: BSD 3 Clause.
 '''
+
+def card(phenny, input):
+	return phenny.say('Perhaps you meant ".card Storm Crow"?')
+card.commands = ['card']
+card.priority = 'medium'
