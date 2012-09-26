3568536a691a18c4a03417972b01d8e8317fa6d9
John Cleaver <cleaver.john.k@gmail.com>
2012-09-25 23:27:47 -0400

    Added the docblock for the module code.

---
+++ card.py
@@ -0,0 +1,7 @@
+'''
+A module for the IRC Bot Phenny that allows for one to get the Oracle text
+for any Magic: the Gathering card by card name.
+
+Author: John Cleaver
+Site: https://github.com/kihashi/mtg-irc
+'''
