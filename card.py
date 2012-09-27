964b1f2b12fde1450bbe160b2b8a7d784aa77a3e
John Cleaver <cleaver.john.k@gmail.com>
2012-09-26 21:51:17 -0400

    Minimal Working copy

--- card.py
+++ card.py
@@ -8,7 +8,38 @@ Site: https://github.com/kihashi/mtg-irc
 License: BSD 3 Clause.
 '''
 
+cards = []
+
 def card(phenny, input):
-	return phenny.say('Perhaps you meant ".card Storm Crow"?')
+    card_list()
+    if not input.group(2):
+        phenny.say('Perhaps you meant ".card Storm Crow"?')
+    else:
+        string = cards[index_containing_substring(cards, input.group(2))]
+        card_listing = string.split("\n")
+        for line in card_listing:
+            phenny.say(line)
+
+
+
 card.commands = ['card']
 card.priority = 'medium'
+
+def card_list():
+    file = open('modules/oracle.txt', 'r')
+
+    internal_entry = ''
+
+    for line in file:
+        if line == '\n':
+            cards.append(internal_entry)
+            internal_entry = ''
+        else:
+            internal_entry += line
+
+def index_containing_substring(the_list, substring):
+    for i, s in enumerate(the_list):
+        if substring in s:
+              return i
+    return -1
+
