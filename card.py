3a763ee8361c1c076a836a97f69e56f99a1e95dc
John Cleaver <cleaver.john.k@gmail.com>
2012-10-07 01:36:30 -0400

    Converted card search to use Tutor Oracle API. Added Ruling Search.

--- card.py
+++ card.py
@@ -7,44 +7,64 @@ Author: John Cleaver
 Site: https://github.com/kihashi/mtg-irc
 License: BSD 3 Clause.
 '''
-import re
-cards = []
+import json
+import urllib
 
+json_url = "http://localhost:3000/card/"
 
 def card(phenny, input):
-    card_list()
     if not input.group(2):
-        phenny.say('Perhaps you meant ".card Storm Crow"?')
+        phenny.say(input.nick + 'Perhaps you meant ".card Storm Crow"?')
     else:
-        index = index_containing_substring(cards, input.group(2))
-        if index < 0:
-            phenny.say("No card was found by that name.")
-            return
-
-        string = cards[index]
-        card_listing = string.replace("\n", " | ")
-        phenny.say(card_listing)
-
+        card_json = get_card_json(input.group(2))
+        if not card_json:
+            phenny.say(input.nick + ": I could not find a card by that name.")
+        else:
+            phenny.say(input.nick + ": " + format_text(card_json))
 card.commands = ['card']
 card.priority = 'medium'
 
-
-def card_list():
-    file = open('modules/oracle.txt', 'r')
-
-    internal_entry = ''
-
-    for line in file:
-        if line == '\n':
-            cards.append(internal_entry)
-            internal_entry = ''
+def rulings(phenny, input):
+    if not input.group(2):
+        phenny.say(input.nick + 'Perhaps you meant ".rulings Storm Crow"?')
+    else:
+        card_json = get_card_json(input.group(2))
+        if not card_json:
+            phenny.say(input.nick + ": I could not find a card by that name.")
         else:
-            internal_entry += line
+            for ruling in format_rulings(card_json):
+                phenny.msg(input.nick, ruling)
+            else:
+                phenny.msg(input.nick, "There are no rulings for that card.")
+rulings.commands = ['rulings']
+rulings.priority = 'medium'
 
+def get_card_json(card):
+    card_url = json_url + card
 
-def index_containing_substring(the_list, substring):
-    for i, s in enumerate(the_list):
-        if re.match(substring + "\n", s, re.IGNORECASE):
-            return i
-    return -1
+    card_json = urllib.urlopen(card_url)
+    card_dict = json.load(card_json)
 
+    if "error" in card_dict:
+        return None
+    else:
+        return card_dict
+
+def format_text(card_dict):
+    output = card_dict['name'] + " | " + card_dict['mana_cost'] + " | "
+
+    for card_type in card_dict['types']:
+        output += card_type + " "
+
+    output += "| " + card_dict['text']
+
+    output = output.replace("\n", " ")
+
+    return output
+
+def format_rulings(card_dict):
+    output = []
+    for ruling in card_dict['rulings']:
+        output.append(ruling[0] + ": " + ruling[1])
+
+    return output
