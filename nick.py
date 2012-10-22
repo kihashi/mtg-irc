b57010b806792e008aa2277dfdfe3965a5a41b9e
John Cleaver <cleaver.john.k@gmail.com>
2012-10-22 00:00:40 -0400

    Added a preliminary list of nicknames

---
+++ nick.py
@@ -0,0 +1,17 @@
+'''
+This module contains a dictionary of card nicknames.
+
+Author: John Cleaver
+License: BSD 3 Clause.
+'''
+
+nicknames = {
+    "Penis Wurm": "",
+    "Bob": "Dark Confidant",
+    "Jens": "Solemn Simulacrum",
+    "Sad Robot": "Solemn Simulacrum",
+    "Skittles": "Skithiryx, the Blight Dragon",
+    "Tim": "Prodigal Sorceror",
+    "Pimp": "Putrid Imp",
+    "The Rock": "hyrexian Negator"
+}
