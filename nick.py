ae4725ce4e121a58dad520292473b7bb2e00e9bb
John Cleaver <cleaver.john.k@gmail.com>
2012-11-07 19:35:40 -0500

    Added a module with a dict of card nicknames/

--- nick.py
+++ nick.py
@@ -6,7 +6,7 @@ License: BSD 3 Clause.
 '''
 
 nicknames = {
-    "Penis Wurm": "",
+    "Penis Wurm": "Armada Wurm",
     "Bob": "Dark Confidant",
     "Jens": "Solemn Simulacrum",
     "Sad Robot": "Solemn Simulacrum",
