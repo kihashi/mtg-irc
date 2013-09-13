b0531824a47b6e115cad548045ce6dc10d5b53b2
John Cleaver <cleaver.john.k@gmail.com>
2013-09-13 16:16:02 +0000

    Added Models for all of the various static tables.

--- card_database/models.py
+++ card_database/models.py
@@ -27,3 +27,45 @@ class Ruling(Entity):
     text = Field(UnicodeText())
     card = ManyToOne('MagicCard')
 
+
+class Color(Entity):
+    color = Field(Unicode(10))
+    cards = ManyToMany('MagicCards')
+
+
+class SuperType(Entity):
+    supertype = Field(Unicode(30))
+    cards = ManyToMany('MagicCard')
+
+    def __repr__(self):
+        return self.supertype
+
+
+class CardType(Entity):
+    cardtype = Field(Unicode(30))
+    cards = ManyToMany('MagicCard')
+
+    def __repr__(self):
+        return self.cardtype
+
+
+class SubType(Entity):
+    subtype = Field(Unicode(30))
+    cards = ManyToMany('MagicCard')
+
+    def __repr__(self):
+        return self.subtype
+
+
+class Rarity(Entity):
+    rarity = Field(Unicode(10))
+    abbreviation = Field(Unicode(1))
+    cards = OneToMany
+
+    def __repr__(self):
+        return self.abbreviation
+
+class Expansion(Entity):
+    expansion = Field(Unicode(30))
+    abbreviation = Field(Unicode(10))
+    cards = ManyToMany('MagicCard')
