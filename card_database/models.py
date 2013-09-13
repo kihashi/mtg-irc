85580bf2f5c1d6a74d290426de1c38db8602b87f
John Cleaver <cleaver.john.k@gmail.com>
2013-09-13 15:25:33 +0000

    Changed DB model to have a single Card table.

--- card_database/models.py
+++ card_database/models.py
@@ -4,37 +4,20 @@ metadata.bind = "sqlite:///cards.sqlite"
 metadata.bind.echo = True
 
 class MagicCard(Entity):
-    using_options(inheritance='multi')
+    layout = ManyToOne('Layout')
     name = Field(Unicode(50))
-    card_types = ManyToMany('CardType')
-    expansions = ManyToMany('Expansion')
-    rarity = ManyToOne('Rarity')
-    supertypes = ManyToMany('SuperType')
-    subtypes = ManyToMany('SubType')
+    alt_side = OneToOne('MagicCard')
     mana_cost = Field(Unicode(30))
+    converted_mana_cost = Field(Integer)
+    colors = ManyToMany('Color')
+    supertypes = ManyToMany('SuperType')
+    card_types = ManyToMany('CardType')
+    subtypes = ManyToMany('SubType')
+    rarity = ManyToOne('Rarity')
     rules_text = Field(UnicodeText())
-
-    def __repr__(self):
-        sets_string = ''
-        for key, value in self.expansions.iteritems():
-            if sets_string != '':
-                sets_string += ', '
-            sets_string += key + '-' + value
-
-        return "{NAME} | {MANA} | {TYPES} |" + \
-               " {RULES} | {SETS}".format(NAME=self.name,
-                                          MANA=self.mana_cost,
-                                          SUPERTYPE=supertype,
-                                          TYPE=self.card_type,
-                                          RULES=self.rules_text,
-                                          SETS=sets_string)
-
-
-class CreatureCard(MagicCard):
-    using_options(inheritance='multi')
-    power = Field(Integer)
-    toughness = Field(Integer)
-
-    def __repr__(self):
-        return MagicCard.__repr__(self) + " | {POWER}/{TOUGHNESS}".format(POWER=self.power,
+    power = Field(Unicode(30))
+    toughness = Field(Unicode(30))
+    loyalty = Field(Integer)
+    expansions = ManyToMany('Expansion')
+    rulings = ManyToMany('Ruling')
 
