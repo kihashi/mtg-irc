4b58692d5195a053f474a53eae80631ed6452162
John Cleaver <cleaver.john.k@gmail.com>
2013-10-10 18:44:07 +0000

    Added colors and supertypes to the parser.

--- mtgjson.py
+++ mtgjson.py
@@ -35,5 +35,19 @@ def load_card(card_json):
     if 'loyalty' in card_json:
         db_card.loyalty = card_json['loyalty']
 
+    if 'colors' in card_json:
+        for card_color in card_json['colors']:
+            db_color = models.Color.get_by(color=card_color)
+            if not db_color:
+                db_color = models.Color(color=card_color)
+            db_card.colors.append(db_color)
+
+    if 'supertypes' in card_json:
+        for card_supertype in card_json['supertypes']:
+            db_supertype = models.SuperType.get_by(supertype=card_supertype)
+            if not db_supertype:
+                db_supertype = models.SuperType(supertype=card_supertype)
+            db_card.supertype.append(db_supertype)
+
 
 def determine_alt_side(name, names):
