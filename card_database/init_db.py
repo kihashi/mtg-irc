f454f44f884a1f6b0aeca9548081fe98d3633d6a
John Cleaver <cleaver.john.k@gmail.com>
2013-10-12 15:15:31 -0400

    Added a database initialization file that loads colors, rarities, and sets.

---
+++ card_database/init_db.py
@@ -0,0 +1,38 @@
+'''
+init_db.py
+A module to set up sets, colors, rarities, and other objects in the database.
+Author: John Cleaver <cleaver.john.k@gmail.com>
+License: BSD 3-Clause
+'''
+
+import models
+from expansion import *
+
+
+def create_colors():
+    for c in ["White", "Blue", "Red", "Black", "Green"]:
+        models.Color(color=c, abbreviation=c[0])
+
+
+def create_rarities():
+    for r in [("Common", "C"),
+              ("Uncommon", "UC"),
+              ("Rare", "R"),
+              ("Mythic Rare", "M")]:
+        models.Rarity(rarity=r[0], abbreviation=r[1])
+
+
+def create_expansions():
+    for e, a in sets.iteritems():
+        models.Expansion(expansion=e, abbreviation=a)
+
+
+def main():
+    models.setup()
+    create_colors()
+    create_rarities()
+    create_expansions()
+    models.close()
+
+if __name__ == '__main__':
+    main()
