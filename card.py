a319ecf1c06dc2dbe2551b4c1582c9793edd617d
John Cleaver <cleaver.john.k@gmail.com>
2014-02-03 18:15:17 -0500

    Card.py to Willie (WIP)

--- card.py
+++ card.py
@@ -1,47 +1,45 @@
 '''
-card.py - Phenny Magic: the Gathering Card Lookup Module
-A module for the IRC Bot Phenny that allows for one to get the Oracle text
+card.py - Willie Magic: the Gathering Card Lookup Module
+A module for the IRC Bot Willie that allows for one to get the Oracle text
 for any Magic: the Gathering card by card name.
 
 Author: John Cleaver
 Site: https://github.com/kihashi/mtg-irc
 License: BSD 3 Clause.
 '''
-from modules import nick
-import re
 
-def card(phenny, input):
-    """Gets the text for a specified card."""
-    if not input.group(2):
-        phenny.say(input.nick + 'Perhaps you meant ".card Storm Crow"?')
+import willie
+import card_database as db
+
+
+@willie.module.commands('card')
+def card(bot, trigger):
+    '''
+    Returns the oracle text of the specified card as a reply to the user.
+
+    Example:
+    .card Storm Crow
+    Storm Crow | {1}{U} | Creature  - Bird | Flying (This creature can't be blocked except by creatures with flying or reach.) | 1/2
+    '''
+
+    input = trigger.group(2)
+    if not input:
+        bot.reply("You must specify a card name when using this command.")
+        return
+    input = sanitize(input)
+    #First try an exact match
+    db_card = db.models.MagicCard.get_by(name=input)
+    if db_card:
+        bot.reply(db_card.get_card_text)
     else:
-        card_name = input.group(2).strip().lower().title()
-        if card_name in nick.nicknames:
-            card_name = nick.nicknames[card_name]
-        card_text = get_card(card_name)
-        if card_text:
-            phenny.reply(card_text)
-        else:
-            phenny.reply("I could not find a card by that name.")
-card.commands = ['card']
-card.priority = 'medium'
-card.example = '.card Storm Crow'
+        #Try to find cards that have a similar name
+        #db_card = models.MagicCard.query.filter(models.MagicCard.name.like("%" + input + "%")).all()
+        bot.reply("I could not find a card by that name.")
 
 
-def get_card(card_name):
-    found_card = False
-    card_text = ''
+def sanitize(input):
+    '''
+    Makes sure that the input string is clean.
+    '''
 
-    with open('modules/oracle.txt') as oracle_db:
-        for line in oracle_db.readlines():
-            if found_card:
-                if line == "\n":
-                    return card_text
-                else:
-                    card_text += " | " + line.strip("\n")
-            else:
-                if re.match(card_name + "\n", line, re.IGNORECASE):
-                    found_card = True
-                    card_text = line.strip("\n")
-
-    return None
+    return input
