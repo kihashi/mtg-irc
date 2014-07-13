e5b783827c678aa3e78c85601ea1b0d3db563c16
John Cleaver <cleaver.john.k@gmail.com>
2014-07-13 16:58:25 -0400

    Added an external config file for the price and eprice modules.
    The file is ignored so that sensitive information is not accidentally committed.
    I added an example config file as well.

---
+++ config.example.py
@@ -0,0 +1,15 @@
+'''
+config.py
+
+Holds config values for MTG-IRC modules.
+'''
+
+# This is the secret URL to the MTGOTraders API.
+mtgotraders_api_url = "http://www.mtgotraders.com/api"
+# This is the store URL for MTGO traders.
+mtgotraders_store_url = "http://www.mtgotraders.com/store"
+
+# This is the secret URL that the TCGPlayer Rep assigns you for API access.
+tcgplayer_api_url = "http://www.tcgplayer.com/price_api"
+# This is the partner code assigned with your TCGPlayer API account.
+tcgplayer_partner_code = "MTGTEST"
